6b26ecb4bfaf366a9c4838112eeac87a736a0c14
aditya-sharma1906 <adihyd1906@gmail.com>
2022-01-22 19:54:29 +0530

    I am aditya

---
+++ Project-1-Band name generator.py
@@ -0,0 +1,4 @@
+print("welcome to band name generator")
+city_name=input("what is the name of the city you grew up in?\n")
+pet_name=input("what is your favourite animal?\n")
+print("Your band name could be "+ city_name + " " + pet_name)
